8e8f04f8d6f1c114576ee80ac802fce23735148c
Filipe <filipegajo95@gmail.com>
2022-05-03 12:55:06 +0100

    login_registo continuacao

---
+++ login/login.js
@@ -0,0 +1,19 @@
+// Valores para testes
+let username = "default";
+let pass = "123456";
+
+// Variáveis
+let inputUsername = document.querySelector(".inputUsername");
+let inputPass = document.querySelector(".inputPass");
+
+// Botão de login
+document.querySelector("button").addEventListener("click", function()
+{
+    // alert("OK!");
+
+    // comparar usernames
+    console.log(inputUsername.value);
+    /*if (inputUsername.textContent === username.textContent) alert("OK!");
+    else alert("Errado!");*/
+
+});
