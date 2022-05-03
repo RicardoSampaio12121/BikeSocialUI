c18b6fa95088395e02c07f7b3c9a7b8cf1ea0cae
Filipe <filipegajo95@gmail.com>
2022-05-03 14:57:22 +0100

    Página de login terminada

--- login/login.js
+++ login/login.js
@@ -5,15 +5,41 @@ let pass = "123456";
 // Variáveis
 let inputUsername = document.querySelector(".inputUsername");
 let inputPass = document.querySelector(".inputPass");
+let mensagemUsernameErrado = document.getElementById("erro-username");
+let mensagemPassErrada = document.getElementById("erro-pass");
 
 // Botão de login
 document.querySelector("button").addEventListener("click", function()
 {
-    // alert("OK!");
+    // Comparar usernames----------
+    // username errado
+    if (inputUsername.value != username) 
+    {
+        inputUsername.value = "";
+        inputUsername.style.borderColor = "red";
+        inputPass.value = "";
+        mensagemUsernameErrado.classList.remove("escondido");
 
-    // comparar usernames
-    console.log(inputUsername.value);
-    /*if (inputUsername.textContent === username.textContent) alert("OK!");
-    else alert("Errado!");*/
+        mensagemPassErrada.classList.add("escondido");
+    }
+    // username correto
+    else 
+    {
+        inputUsername.style.borderColor = "black";
+        mensagemUsernameErrado.classList.add("escondido");
 
+        // Comparar passwords----------
+        // pass errada
+        if (inputPass.value != pass)
+        {
+            inputPass.value = "";
+            mensagemPassErrada.classList.remove("escondido");
+        } 
+        // pass correta
+        else 
+        {
+            mensagemPassErrada.classList.add("escondido");
+            alert("Login efetuado com sucesso!");
+        }
+    }
 });
