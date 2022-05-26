321d1cb097fb91a192265a49ef0ba299fdfd49f7
pcpr1994 <75262703+pcpr1994@users.noreply.github.com>
2022-05-26 23:54:05 +0100

    at

--- TP_paula/src/app/compoments/conta/privacy-definitions/privacySettings.ts
+++
@@ -1,9 +0,0 @@
-export interface IPrivacySettings {
-    profileVisibility: number;
-    commentsPermission: number;
-    unfriendContactPermission: boolean;
-    unfriendTrodyVisualization: boolean;
-    privateTrainings: boolean;
-    privateRaces: boolean;
-    privateRoutes: boolean;
-}
