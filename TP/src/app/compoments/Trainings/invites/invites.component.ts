6333d6c9aaa7afe380a16bb97c1a65e183b7bb54
Ricardo <ricardo_cs@outlook.pt>
2022-05-28 20:37:03 +0100

    Parte da Joana-> Invite para treinos

--- TP/src/app/compoments/Trainings/invites/invites.component.ts
+++ TP/src/app/compoments/Trainings/invites/invites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IAvailableTrainings, IInviteTraining, INeededInfo } from './trainingInvites';
+import { TrainingInvitesService } from './trainingInvites-service';
 
 @Component({
   selector: 'app-invites',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvitesComponent implements OnInit {
 
-  constructor() { }
+  sub!: Subscription
+  
+  trainings: Array<IAvailableTrainings>
+
+  info: INeededInfo = {athleteId: 0, username: "", place: "", contact: 0, clubName: "", clubId: 0}
+
+  toAdd: IInviteTraining = { trainingId: 0, athleteId: 0 }
+  
+
+  trainToCheck: number = 0
+
+  constructor(private trainingInvService: TrainingInvitesService) { }
 
   ngOnInit(): void {
+    this.sub = this.trainingInvService.getTrainingsInformation().subscribe({
+      next: trainings => {
+        this.trainings = trainings
+        console.log(this.trainings)
+      }
+    })
+
+    this.sub = this.trainingInvService.getNeededInfo().subscribe({
+      next: neededInfo => {
+        this.info = neededInfo
+      }
+    })
+  }
+
+  confirm() {
+    this.toAdd.trainingId = this.trainToCheck
+    this.toAdd.athleteId = this.info.athleteId 
+
+    this.sub = this.trainingInvService.selfInvite(this.toAdd).subscribe()
+  }
+
+  changeCheckBox(trainId: number) {
+    // Isto está feito à trolha
+    if(trainId == this.trainToCheck) this.trainToCheck = 0
+    else this.trainToCheck = trainId
   }
 
 }
