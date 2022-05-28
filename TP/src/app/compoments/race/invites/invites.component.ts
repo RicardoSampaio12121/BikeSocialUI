12dfbb69e429cf470d20a6899989f4f9d469ccf0
Filipe <filipegajo95@gmail.com>
2022-05-28 22:43:15 +0100

    InviteRaces pt1

--- TP/src/app/compoments/race/invites/invites.component.ts
+++ TP/src/app/compoments/race/invites/invites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IAvailableRaces } from './raceInvites';
+import { RaceInvitesService } from './raceInvites-service';
 
 @Component({
   selector: 'app-invites',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvitesComponentR implements OnInit {
 
-  constructor() { }
+  sub!: Subscription
+
+  races: Array<IAvailableRaces>
+
+  constructor(private raceInvService: RaceInvitesService) { }
 
   ngOnInit(): void {
+    this.sub = this.raceInvService.getRacesInformation().subscribe({
+      next: races => {
+        this.races = races
+        console.log("abc",this.races)
+      }
+    })
   }
 
 }
---
+++ TP/src/app/compoments/race/invites/raceInvites.ts
@@ -0,0 +1,26 @@
+export interface IAvailableRaces{
+    Description: string,
+    Distance: number,
+    EstimatedTime: number,
+    date: string,
+    time: string,
+    Federation: string,
+    RaceType: string,
+    City: string,
+    Town: string,
+    PlaceName: string
+}
+/*
+export interface IInviteRace{
+    raceId: number,
+    athleteId: number
+}
+
+export interface INeededInfo{
+    athleteId: number,
+    username: string,
+    place: string,
+    contact: number,
+    clubName: string,
+    clubId: number
+}*/
